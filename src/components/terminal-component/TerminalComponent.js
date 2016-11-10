909fc465320e87dbe2e65c8b947a3ad94c7848dc
prevosis <aadilzbhatti@gmail.com>
2016-11-09 22:52:02 -0600

    Refactoring

--- src/components/terminal-component/TerminalComponent.js
+++ src/components/terminal-component/TerminalComponent.js
@@ -2,22 +2,11 @@ import React from 'react';
 
 import Terminal from './Terminal';
 
-export default class TerminalComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateTerm = this.updateTerm.bind(this);
-  }
-
-  updateTerm(term) {
-    this.props.updateTerm(term);
-  }
-
-  render() {
-    return (
-      <Terminal
-        width={this.props.width}
-        height={this.props.height}
-        updateTerm={this.updateTerm}/>
-    );
-  }
+export default (props) => {
+  return (
+    <Terminal
+      width={props.width}
+      height={props.height}
+      updateTerm={props.updateTerm} />
+  );
 }
