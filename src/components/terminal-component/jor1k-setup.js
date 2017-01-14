91221f0a60787bb41099b0e059f2ea1f93acffcf
prevosis <aadilzbhatti@gmail.com>
2017-01-14 13:31:25 -0600

    Removed webworker creation

--- src/components/terminal-component/jor1k-setup.js
+++ src/components/terminal-component/jor1k-setup.js
@@ -1,11 +1,12 @@
 import Jor1k from './jor1k/js/master/master';
 import LinuxTerm from './jor1k/js/plugins/terminal-linux';
 
-import MyWorker from 'worker-loader!./jor1k/js/worker/worker';
+// import MyWorker from 'worker-loader!./jor1k/js/worker/worker';
 
 export default () => {
   let termTTY0 = new LinuxTerm('tty0');
-  let webWorker = new MyWorker();
+  // let webWorker = new MyWorker();
+  let webWorker = new Worker('jor1k-worker-min.js');
 
   return new Jor1k(
     {
